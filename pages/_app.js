adc65aadeda27924790468f9bbe79ed426f0d244
kks91 <kokyusik91@naver.com>
2022-09-19 14:49:21 +0900

    🚧 Implement Main navigation and logo components

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
 import '../styles/globals.css';
+import Layout from '../components/layout/layout';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <Layout>
+      {/* Layout 컴포넌트의 children이 된다. */}
+      <Component {...pageProps} />
+    </Layout>
+  );
 }
 
 export default MyApp;
