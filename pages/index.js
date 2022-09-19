29f15b35681c3608598298ff51403af8edbde760
kks91 <kokyusik91@naver.com>
2022-09-19 14:47:51 +0900

    🚧 Init Next.js Pages components

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Hero from '../components/home-page/hero';
+
+function HomePage() {
+  return (
+    <>
+      <Hero />
+      {/* <FeaturedPost /> */}
+    </>
+  );
+}
+
+export default HomePage;
+
+// 1) Hero section : 환영 섹션으로 주로 상품을 소개함. 블로그 소개
+
+// 2) Featured Posts : 추천 게시물
