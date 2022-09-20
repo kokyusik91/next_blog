52a917904785985a2b38ae446f9f9a5e565d8020
kks91 <kokyusik91@naver.com>
2022-09-20 12:04:51 +0900

    ✨ Implement Detail Post Page by using Node.js file System

--- components/posts/post-detail/post-content.js
+++ components/posts/post-detail/post-content.js
@@ -2,22 +2,15 @@ import PostHeader from './post-header';
 import classes from './post-content.module.css';
 import ReactMarkdown from 'react-markdown';
 
-const DUMMY_POST = {
-  title: 'Getting started with nextjs',
-  image: 'getting-started-nextjs.png',
-  slug: 'getting-started-with-nextjs',
-  date: '2022-02-10',
-  content: '# This is a first Post ☺️',
-};
-
-function PostContent() {
-  const imagePath = `/images/posts/${DUMMY_POST.slug}/${DUMMY_POST.image}`;
+function PostContent(props) {
+  const { post } = props;
+  const imagePath = `/images/posts/${post.slug}/${post.image}`;
 
   return (
     <article className={classes.content}>
-      <PostHeader title={DUMMY_POST.title} image={imagePath} />
+      <PostHeader title={post.title} image={imagePath} />
       {/* 실제 마크다운 코드가 JSX로 변환되어 들어온다 */}
-      <ReactMarkdown>{DUMMY_POST.content}</ReactMarkdown>
+      <ReactMarkdown>{post.content}</ReactMarkdown>
     </article>
   );
 }
--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -1,7 +1,36 @@
 import PostContent from '../../components/posts/post-detail/post-content';
+import { getPostData, getPostsFiles } from '../../lib/posts-util';
 
-function PostDetailPage() {
-  return <PostContent />;
+function PostDetailPage(props) {
+  return <PostContent post={props.post} />;
+}
+
+export function getStaticProps(context) {
+  // params를 통해 구체적인 slug 값을 추출할 수 있다.
+  const { params } = context;
+  const { slug } = params;
+
+  const postData = getPostData(slug);
+
+  return {
+    props: {
+      post: postData,
+    },
+    // 600초 마다 재배포 === 10분마다 한번씩 최신 데이터를 가지고 온다.
+    revalidate: 600,
+  };
+}
+
+// 미리 생성해야하는 페이지들의 경로를 가지는 객체 즉 구체적인 slug값을 갖는다.
+export function getStaticPaths() {
+  // 어떤 파일 경로가 있는지 가져온다.
+  const postFilenames = getPostsFiles();
+  // 파일 경로를 가져와서 뒤에 md 확장자를 띄어서 배열로 만든다.
+  const slugs = postFilenames.map((fileName) => fileName.replace(/\.md$/, ''));
+  return {
+    paths: slugs.map((slug) => ({ params: { slug: slug } })),
+    fallback: false,
+  };
 }
 
 export default PostDetailPage;
