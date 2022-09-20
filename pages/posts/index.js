cc3f2b40068309318be765c0028f25de22308fe0
kks91 <kokyusik91@naver.com>
2022-09-20 10:05:21 +0900

    ✨ Implement All Post page

--- pages/posts/index.js
+++ pages/posts/index.js
@@ -1,3 +1,18 @@
-function AllPostPage() {}
+import AllPosts from '../../components/posts/all-posts';
+import { getAllPosts } from '../../lib/posts-util';
+
+function AllPostPage(props) {
+  return <AllPosts posts={props.posts} />;
+}
+
+export function getStaticProps() {
+  const allPosts = getAllPosts();
+
+  return {
+    props: {
+      posts: allPosts,
+    },
+  };
+}
 
 export default AllPostPage;
