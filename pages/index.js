d42965f3bd45898e1f559ca260360d230656d234
kks91 <kokyusik91@naver.com>
2022-09-20 18:07:59 +0900

    🎨  Head Component

--- pages/index.js
+++ pages/index.js
@@ -2,9 +2,18 @@ import Hero from '../components/home-page/hero';
 import FeaturedPost from '../components/home-page/featured-posts';
 import { getFeaturedPosts } from '../lib/posts-util';
 
+import Head from 'next/head';
+
 function HomePage(props) {
   return (
     <>
+      <Head>
+        <title>Ko' Blog</title>
+        <meta
+          name="description"
+          content="I post about programming and web dev"
+        />
+      </Head>
       <Hero />
       <FeaturedPost posts={props.posts} />
     </>
